ff98355eec0096336a43abe803348b15b9c3bbbb
Yamin <yamin@MacBook-Pro-de-Yamin.local>
2024-03-02 18:02:58 +0100

    clean accessible view

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    # path('', views.home, name='home'),
+    # path('register/', views.register, name='register'),
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     
     path('logout/', views.logout_view, name='logout'),
