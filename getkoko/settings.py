3f6a80212842eb494ba0c2a027ea66e94a62b6ce
Yamin <yamin@MacBook-Pro-de-Yamin.local>
2024-03-02 18:47:25 +0100

    test static

--- getkoko/settings.py
+++ getkoko/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'core/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
+# STATIC_URL = 'core/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static'),]
 
 # Default primary key field type
