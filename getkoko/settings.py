86996e0657ba79c98a9d3b36cb0b5436b25b50db
Yamin <yamin@mbp-de-yamin.home>
2024-03-05 20:19:53 +0100

    deploy test

--- getkoko/settings.py
+++ getkoko/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
